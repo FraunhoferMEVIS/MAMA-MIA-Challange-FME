b288e9e323b35b77648861e12194b54a465e1e09
Kai Geissler <kai.geissler@mevis.fraunhofer.de>
2025-06-28 09:36:56 +0200

    Updated config parsing to fit new search space

--- hyperparameter_tuning/tune_random_search_2d.py
+++ hyperparameter_tuning/tune_random_search_2d.py
@@ -10,15 +10,16 @@ def run_fold(sampled_config, fold_idx):
     with open(os.environ["BASE_CONFIG"], 'r') as f:
         config = json.load(f)
 
-    # TODO: Update search space with new parameters and start new run
     config.update({
+        "optimizer": sampled_config["optimizer"],
         "learning_rate": sampled_config["learning_rate"],
+        "final_learning_rate": sampled_config["final_learning_rate"],
+        "momentum": sampled_config["momentum"],
         "weight_decay": sampled_config["weight_decay"],
         "batch_size": int(sampled_config["batch_size"]),
         "label_smoothing": sampled_config["label_smoothing"],
         "target_size": (int(sampled_config["x_y_resolution"]),
                         int(sampled_config["x_y_resolution"])),
-        "normalization": sampled_config["normalization"],
         "model_key": sampled_config["model_key"],
         "data_split_file": f"fold_{fold_idx}.json",
     })
@@ -45,20 +46,20 @@ def train_func(config):
         # Handle OOM configurations by reporting a small metric value
         tune.report({"mean_5_fold_ranking_score": 0, "balanced_accuracy": 0})
 
-search_space = {
-    "optimizer": tune.choice(["sgd", "adamw"]),
-    "learning_rate": tune.loguniform(1e-4, 1e-1),
-    "final_learning_rate": tune.loguniform(1e-8, 1e-2),
-    "momentum": tune.uniform(0.8, 0.98),
-    "weight_decay": tune.loguniform(1e-6, 1e-1),
-    "batch_size": tune.quniform(8, 48, 1),
-    "label_smoothing": tune.loguniform(0.0001, 0.1),
-    "x_y_resolution": tune.quniform(50, 200, 1),
-    "model_key": tune.choice(["convnext_tiny", "efficientnet_b2", "efficientnet_b3", "efficientnet_b4",
-                              "efficientnet_b5", "resnet18", "resnet34", "resnet50", "swin_t", "swin_v2_t"])
-}
-
 if __name__ == "__main__":
+    search_space = {
+        "optimizer": tune.choice(["sgd", "adamw"]),
+        "learning_rate": tune.loguniform(1e-4, 1e-1),
+        "final_learning_rate": tune.loguniform(1e-8, 1e-2),
+        "momentum": tune.uniform(0.8, 0.98),
+        "weight_decay": tune.loguniform(1e-6, 1e-1),
+        "batch_size": tune.quniform(8, 48, 1),
+        "label_smoothing": tune.loguniform(0.0001, 0.1),
+        "x_y_resolution": tune.quniform(50, 200, 1),
+        "model_key": tune.choice(["convnext_tiny", "efficientnet_b2", "efficientnet_b3", "efficientnet_b4",
+                                "efficientnet_b5", "resnet18", "resnet34", "resnet50", "swin_t", "swin_v2_t"])
+    }
+
     trainable_with_resources = tune.with_resources(train_func, {"cpu": 4, "gpu": 1})
     name = os.environ["TUNING_RUN_NAME"]
     storage_path = os.environ["TUNE_RESULTS"]
